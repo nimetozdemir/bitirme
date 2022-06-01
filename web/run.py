12967d0673d45fcf876b578fe2639fc7a0fb39a5
ali gökkaya <43973787+aligokkaya@users.noreply.github.com>
2022-06-02 00:04:46 +0300

    update hype_emre

--- web/run.py
+++ web/run.py
@@ -4,11 +4,6 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0',port='5000', debug=True)
 
     
-    
-    
-    
-    
-    
 
 #/usr/local/bin/snscrape
  #   /usr/local/lib/python3.9/dist-packages/snscrape-0.3.4.dist-info/*
